2b3a5c796c1e276c33d5de3bc30aaee1717e8cf6
devinrgit <devin.git.mail@gmail.com>
2024-10-11 17:16:16 -0400

    had screen variable outside main() - fixed

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -15,5 +16,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
