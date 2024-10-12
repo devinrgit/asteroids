6d0f86b650f657d9b292e377ccdb7f5e2003c860
devinrgit <devin.git.mail@gmail.com>
2024-10-12 13:49:34 -0400

    added left and right rotation

--- main.py
+++ main.py
@@ -18,11 +18,17 @@ def main():
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return 
+                return
+             
+        player.update(dt)
+
         screen.fill(000000)
         player.draw(screen)
         pygame.display.flip()
+
+        # limit framerate to 60 FPS
         dt = clock.tick(60) / 1000
+        
 
 if __name__ == "__main__":
     main()
