ce14115b647a75a2006a39b300f204df17546600
devinrgit <devin.git.mail@gmail.com>
2024-10-12 18:59:11 -0400

    Added asteroids and collision check

--- circleshape.py
+++ circleshape.py
@@ -20,3 +20,7 @@ class CircleShape(pygame.sprite.Sprite):
     def update(self, dt):
         # sub-classes must override
         pass
+
+    def collision_check(self, object):
+        distance = pygame.math.Vector2.distance_to(self.position, object.position)
+        return distance <= (self.radius + object.radius)
--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import pygame
 from constants import *
-from circleshape import *
-from player import *
+from circleshape import CircleShape
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+import sys
 
+updatable_group = pygame.sprite.Group()
+drawable_group = pygame.sprite.Group()
+asteroids = pygame.sprite.Group()
+
+Player.containers = (updatable_group, drawable_group)
+Asteroid.containers = (asteroids, updatable_group, drawable_group)
+AsteroidField.containers = (updatable_group)
 
 def main():
     pygame.init()
@@ -10,6 +20,7 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroidfield = AsteroidField()
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -19,11 +30,23 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-             
-        player.update(dt)
 
-        screen.fill(000000)
-        player.draw(screen)
+        # Player movement
+        for obj in updatable_group:
+            obj.update(dt)
+
+        for obj in asteroids:
+            if player.collision_check(obj):
+                sys.exit("Game over!")
+
+        # Display black screen
+        screen.fill((0, 0, 0))
+
+        # Draw objects
+        for obj in drawable_group:
+            obj.draw(screen)
+
+        # Flip display
         pygame.display.flip()
 
         # limit framerate to 60 FPS
